d0c94e70e09e0e1ed86bb3220a790297c1ae6a88
Tim Reinhart <tim.reinhart@bluewin.ch>
2023-12-21 02:38:36 -0800

    changed logic the create the dataset

--- create_dataset.py
+++ create_dataset.py
@@ -1,24 +1,33 @@
 import json
+import os
 
 from util import generate_examples, save_img
 
 if __name__ == "__main__":
     NOISE_LEVEL = 0.3
     IMG_SIZE = 100
-    NR_OF_EXAMPLES = 1000
+    NR_OF_EXAMPLES = [5000, 500, 500]
+    SET_NAMES = ["train", "test", "val"]
 
-    count = 0
-    labels = {}
+    for i, set_name in enumerate(SET_NAMES):
 
-    for img, params in generate_examples(NOISE_LEVEL, IMG_SIZE):
-        name = f"img_{count}.png"
-        labels[name] = params._asdict()
-        save_img(img, "data/" + name)
-        count += 1
+        data_dir = f"data/{set_name}"
 
-        if count > NR_OF_EXAMPLES:
-            break
+        if not os.path.exists(data_dir):
+            os.makedirs(data_dir)
 
-    with open("data/dataset.json", "w") as f:
-        json.dump(labels, f)
-    
+        count = 0
+        labels = {}
+
+        for img, params in generate_examples(NOISE_LEVEL, IMG_SIZE):
+            name = f"img_{count}.png"
+            labels[name] = params._asdict()
+            img_path = f"{data_dir}/{name}"
+            save_img(img, img_path)
+            count += 1
+
+            if count > NR_OF_EXAMPLES[i]:
+                break
+
+        with open(f"{data_dir}/dataset.json", "w") as f:
+            json.dump(labels, f)
